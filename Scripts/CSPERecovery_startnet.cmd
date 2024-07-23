2c0a535289eaffd7364498d2eaa5b27ebeae0be9
Kellen Arb <kellen.arb@crowdstrike.com>
2024-07-22 23:31:19 -0500

    Updated to version 1.1

--- Scripts/CSPERecovery_startnet.cmd
+++ Scripts/CSPERecovery_startnet.cmd
@@ -1,14 +1,14 @@
 @echo off
-echo "CrowdStrike Preinstallation Environment Recovery Utility"
-echo "********************************************************"
-echo "[+] Starting Windows PE"
+echo CrowdStrike Preinstallation Environment Recovery Utility
+echo ********************************************************
+echo [+] Starting Windows PE
 wpeinit
 
-echo "[+] Changing to the high performance power plan"
+echo [+] Changing to the high performance power plan
 powercfg /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
 
-echo "[+] Launching Repair Tool"
+echo [+] Launching Repair Tool
 powershell -executionpolicy Unrestricted -noexit -File ".\CSPERecovery.ps1"
 
-echo "[+] Rebooting"
+echo [+] Rebooting
 wpeutil reboot
--- Scripts/SafeBoot_startnet.cmd
+++ Scripts/SafeBoot_startnet.cmd
@@ -1,17 +1,18 @@
 @echo off
-echo "CrowdStrike Safe Mode Boot Utility"
-echo "**********************************"
-echo "[+] Starting Windows PE"
+echo CrowdStrike Safe Mode Boot Utility
+echo **********************************
+
+echo [+] Starting Windows PE
 wpeinit
 
-echo "[+] Enabling Safe Mode with Networking"
+echo [+] Enabling Safe Mode with Networking
 bcdedit /set {default} safeboot network
 
-echo "[+] Rebooting into Windows in 5 seconds"
-echo "    Do not boot from CD/USB again, and allow Windows to boot up in Safe Mode"
+echo [+] Rebooting into Windows in 5 seconds
+echo     Do not boot from CD/USB again, and allow Windows to boot up in Safe Mode
 
 REM timeout command does not work in WinPE, so we use PowerShell to run Start-Sleep -Seconds 5
 powershell -ExecutionPolicy Unrestricted -EncodedCommand "UwB0AGEAcgB0AC0AUwBsAGUAZQBwACAALQBTAGUAYwBvAG4AZABzACAANQA="
 
-echo "[+] Rebooting"
+echo [+] Rebooting
 wpeutil reboot
