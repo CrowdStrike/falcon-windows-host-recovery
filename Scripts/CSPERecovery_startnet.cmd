0215c2cd96ad8f1e615a179be16e06e342979691
Chris Hammond <chris.hammond@crowdstrike.com>
2024-07-26 21:57:37 +0100

    Updates the code to version 1.3.1

--- Scripts/CSPERecovery_startnet.cmd
+++ Scripts/CSPERecovery_startnet.cmd
@@ -7,8 +7,11 @@ wpeinit
 echo [+] Changing to the high performance power plan
 powercfg /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
 
+echo [+] Trusting CrowdStrike's Authenticode code signing certificate in Windows PE
+certutil -f -addstore TrustedPublisher .\CSAuthenticodePublicKey.pem
+
 echo [+] Launching Repair Tool
-powershell -executionpolicy Unrestricted -noexit -File ".\CSPERecovery.ps1"
+powershell -ExecutionPolicy RemoteSigned -noexit -File ".\CSPERecovery.ps1"
 
 echo [+] Rebooting
 wpeutil reboot
